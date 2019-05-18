537fc64c7d584595421bdb49e20166ec89c509ff
Corvo Han <h39han@edu.uwaterloo.ca>
2019-05-18 00:56:34 -0400

    Update Image upload

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
+const multer = require("multer");
 const formidable = require('formidable');
 const fs = require('fs');
+const path = require('path');
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json('a');
+});
+const upload = multer({
+  dest:"D:/image"
+});
 
-router.post('/imageUpload', function(req, res, next) {
-  console.log(req);
+router.post('/imageUpload',upload.single("pic"), function(req, res, next) {
+  const tempPath = req.file.path;
+  console.log(tempPath);
+  var targetPath = path.join("D:/image", req.file.originalname);
+  console.log(targetPath);
+  fs.rename(tempPath,targetPath);
   res.json('abc');
 });
 
