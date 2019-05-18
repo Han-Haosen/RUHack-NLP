cd06443bd7f4db0b8d0dbc44d186041e688391b5
haosenhan <haosen.han@empire.ca>
2019-05-18 00:13:25 -0400

    Update Server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser');
+const formidable = require('formidable');
+const fs = require('fs');
 /* GET home page. */
 
 router.post('/imageUpload', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req);
+  res.json('abc');
 });
 
 module.exports = router;
