8e0853d223c60a7d6f8cc29c98a0471f0431e49a
Corvo Han <h39han@edu.uwaterloo.ca>
2019-05-18 01:38:52 -0400

    Updating functions

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const multer = require("multer");
-const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path');
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json('a');
@@ -18,6 +19,9 @@ router.post('/imageUpload',upload.single("pic"), function(req, res, next) {
   var targetPath = path.join("D:/image", req.file.originalname);
   console.log(targetPath);
   fs.rename(tempPath,targetPath);
+  const [result] = client.documentTextDetection(fileName);
+  const fullTextAnnotation = result.fullTextAnnotation; 
+console.log(`Full text: ${fullTextAnnotation.text}`)
   res.json('abc');
 });
 
