eadabf7e96b58aa0b13efddfd57aae13e3c1af26
Shrek <faheem5948@gmail.com>
2019-05-18 05:15:02 -0400

    Added stats to the json
    wordCount, sentenceCount, averageReadTime

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,27 @@
-var express = require('express');
-var router = express.Router();
+// imports
+const express = require('express');
+const router = express.Router();
 const multer = require("multer");
 const fs = require('fs');
 const path = require('path');
-var unirest = require('unirest');
+const unirest = require('unirest');
+// app const
+const app = express();
+
+// setting local
+app.use(express.static('/home/faheem/Desktop/ruhacks/RUHack-NLP-Backend/server/public/html')); 
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json('a');
+  fs.readFile(__dirname + '/html/index.html', 'utf8', (err, text) => {
+    res.send(text);
+  });
 });
+
 const upload = multer({
-  dest: "D:/image"
+  dest: "/home/faheem/Desktop/ruhacks/RUHack-NLP-Backend/server/images/"
 });
+
 async function getText(targetPath) {
   // Imports the Google Cloud client library
   const vision = require('@google-cloud/vision');
@@ -19,15 +30,14 @@ async function getText(targetPath) {
   const client = new vision.ImageAnnotatorClient();
 
   // Performs label detection on the image file
-
   const [result] = await client.documentTextDetection(targetPath);
   const fullTextAnnotation = result.fullTextAnnotation;
-  console.log(`Full text: ${fullTextAnnotation.text}`);
   var sentenceCount = Math.floor(fullTextAnnotation.text.split(".").length / 2);
   let textSent = { "key": '7338c0f741743c862fee7b5ec0a2db78', "txt": fullTextAnnotation.text, "sentences": sentenceCount };
   textSent = JSON.stringify(textSent);
   return textSent;
 }
+
 function summarize(textSent) {
   return new Promise((resolve, reject) => {
     var returnValue = {};
@@ -35,28 +45,47 @@ function summarize(textSent) {
       .header("Content-Type", "application/json")
       .send(textSent)
       .end(function (result) {
-        console.log(result.body.summary);
         returnValue.text = JSON.parse(textSent).txt;
         returnValue.summary = result.body.summary;
-        console.log(returnValue);
         return resolve(returnValue);
       });
   })
-
 }
+
+function clean_json(returnValue){
+  // creating a new json with stats
+  var newReturnValue = {
+    original:{
+      text: returnValue.text,
+      stats:{
+        wordCount: returnValue.text.split(' ').length,
+        sentenceCount: returnValue.text.split('.').length,
+        averageReadTime: Math.floor(returnValue.text.length / 500)
+      }
+    },
+    summerized:{
+      text: returnValue.summary,
+      stats:{
+        wordCount: returnValue.summary.split(' ').length,
+        sentenceCount: returnValue.summary.split('.').length,
+        averageReadTime: Math.floor(returnValue.summary.length / 500)
+      }
+    }
+  }
+  console.log(newReturnValue);
+  return newReturnValue;
+}
+
 router.post('/imageUpload', upload.single("pic"), function (req, res, next) {
   const tempPath = req.file.path;
-  console.log(tempPath);
-  var targetPath = path.join("D:/image", req.file.originalname);
-  console.log(targetPath);
+  var targetPath = path.join("", req.file.originalname);
   fs.rename(tempPath, targetPath, () => {
     getText(targetPath).then((value) => {
       summarize(value).then((returnValue) => {
-        res.json(returnValue);
+        res.json(clean_json(returnValue));
       })
     });
   });
-
 });
 
 module.exports = router;
